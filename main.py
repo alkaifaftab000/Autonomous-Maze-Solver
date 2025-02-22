9a91d23fd1bc726fd639e606bf52ae571076c12a
alkaifaftab000 <alkaif.aftab000@outlook.com>
2025-02-22 21:22:39 +0530

    part 7 completed

--- main.py
+++ main.py
@@ -2,14 +2,32 @@
 import gymnasium as gym
 import gymnasium_robotics
 import numpy as np
+from agent import Agent
+from buffer import ReplayBuffer
 from gym_robotics_custom import RoboGymObservationWrapper  # Import the wrapper class
 
 # Register robotics environments
 gymnasium_robotics.register_robotics_envs()
 
 if __name__ == '__main__':
+
+    replay_buffer_size = 1000000
+    episodes = 1000
+    warmup = 64
+    batch_size = 256
+    updates_per_step = 1
+    gamma = 0.99
+    tau = 0.99
+    alpha = 0.12
+    target_update_interval = 1
+    hidden_size = 512
+    learning_rate = 0.0001
+    exploration_scaling_factor = 1.5
+    # evaluate = False
+
     env_name = "PointMaze_Large-v3"
     max_episode_steps = 10000000
+
 
     # Defining structure of the maze
     STRAIGHT_MAZE =    [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
@@ -25,7 +43,7 @@ if __name__ == '__main__':
     env = gym.make(
         env_name,
         max_episode_steps=max_episode_steps,
-        render_mode="human",
+        # render_mode="human",
         maze_map=STRAIGHT_MAZE
     )
 
@@ -35,6 +53,24 @@ if __name__ == '__main__':
     # Reset the environment
     observation, info = env.reset()
 
+    observation_size = observation.shape[0]
+
+    # Agent
+    agent = Agent(
+        observation_size,
+        env.action_space.shape,
+        hidden_size,
+        replay_buffer_size,
+        gamma =gamma,
+        tau = tau,
+        alpha=alpha,
+        target_update_interval=target_update_interval,
+        hidden_size=hidden_size,
+        learning_rate=learning_rate,
+        exploration_scaling_factor=exploration_scaling_factor
+    )
+
+    memory = ReplayBuffer(replay_buffer_size,input_size=observation_size,n_actions=env.action_space.shape[0])
     # Run for 100 steps
     for i in range(max_episode_steps):
         action = env.action_space.sample()  # Sample a random action
